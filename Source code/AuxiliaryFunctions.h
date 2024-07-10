23314f8fe32307fa7895c0795480dbfa3caec592
BVasili <bvasiliev@icloud.com>
2024-07-10 22:39:24 +0300

    merged assist_func to axuilaryfunctions

--- Source code/AuxiliaryFunctions.h
+++ Source code/AuxiliaryFunctions.h
@@ -1,3 +1,18 @@
 #pragma once
 
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define MENU_DEFAULT -1
+
+//loadDoctors
+//loadLine
+//updatefiles
+//searchPatient
+//displayError
+//case2assignDoctor
+int printMenu(void);
 void loadPatients();
