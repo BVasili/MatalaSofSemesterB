ae6086ae32ed6eb90036f0cbddbc12abdae20bcf
shalev shemesh <RaamShal.ShemeshM@ruppin365.net>
2024-07-14 20:18:25 +0300

    l

--- Source code/AuxiliaryFunctions.c
+++ Source code/AuxiliaryFunctions.c
@@ -15,19 +15,19 @@ int printMenu(void)
 	while (action < 0 || action>12)
 	{
 		printf("MENU:\n\n\
-     0. Exit Program\n\n\
-     1. Admit Patient\n\n\
-     2. Check fot patient's allergies\n\n\
-     3. Dispaly all patients\n\n\
-     4. Dispaly all patient's admission\n\n\
-     5. Dispaly all patients in line\n\n\
-     6. Advance patient in line\n\n\
-     7. Display list of all doctors\n\n\
-     8. Display all patients assigned to a doctor\n\n\
-     9. Discharge patient\n\n\
-     10. Remove visit\n\n\
-     11. Remove patient\n\n\
-     12. Close the hospital\n");
+  #   0. Exit Program\n\n\
+  #   1. Admit Patient\n\n\
+  #   2. Check fot patient's allergies\n\n\
+  #   3. Dispaly all patients\n\n\
+  #   4. Dispaly all patient's admission\n\n\
+  #   5. Dispaly all patients in line\n\n\
+  #   6. Advance patient in line\n\n\
+  #   7. Display list of all doctors\n\n\
+  #   8. Display all patients assigned to a doctor\n\n\
+  #   9. Discharge patient\n\n\
+  #   10. Remove visit\n\n\
+  #   11. Remove patient\n\n\
+  #   12. Close the hospital\n");
 		printf("\n\nplease enter a the number of the wanted action:\t");
 		fseek(stdin, 0, SEEK_END);
 		scanf("%d", &action);
