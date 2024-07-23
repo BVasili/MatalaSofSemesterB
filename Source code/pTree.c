11ec446d63d4be13bd5b55e3ca1410d5564a6af9
shalev shemesh <RaamShal.ShemeshM@ruppin365.net>
2024-07-23 22:42:12 +0300

    rek oved

--- Source code/pTree.c
+++ Source code/pTree.c
@@ -114,3 +114,4 @@ pInTree* searchPatient(pTree* tree, char* ID)
 {
 	return searchPatientInTree(tree->root, ID);
 }
+
--- Source code/updateFiles.h
+++ Source code/updateFiles.h
@@ -9,3 +9,7 @@
 #include"displayError.h"
 
 void updateFiles(char*, char*, char*, List*, pLine*, pTree*);
+void writePatientQueueToFile(FILE* file, pLine* queue);
+void writeDoctorsToFile(FILE* file, List* doctorList);
+void writePatientToFile(FILE* file, Patient* patient, int index);
+void writePatientsFromTree(FILE* file, pInTree* node, int* index);
