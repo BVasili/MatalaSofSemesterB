90fbfe6ecadba416d69c931dafa671fba6b212c7
BVasili <bvasiliev@icloud.com>
2024-07-25 22:21:31 +0300

    commit!

--- Source code/assignDoctor2case.h
+++ Source code/assignDoctor2case.h
@@ -7,3 +7,4 @@ Node* assignDoc2Case(List*);
 Node* insertSorted(Node* head, Node* node);
 Node* removeNode(Node* head, Node* node);
 Node* assignDoc2Case(List* DoctorsList);
+#endif //ASSIGNDOCTOR2CASE__
